cyberrumor/ammo

commit 90a53f1fd9b584fd21d8bf15d9d1616ce2c76d9a

Make UI class show example commands in help menu
The help menu now shows example commands for commands that accept
arguments.

Also, add more help text to the find command to clarify that you can
execute it without arguments to remove a filter that was applied by a
find command executed _with_ args.

ammo/ui.py
@@ -20,6 +20,7 @@ from abc import (
     abstractmethod,
 )
 from dataclasses import dataclass
+from itertools import product
 
 
 class Controller(ABC):
@@ -80,6 +81,7 @@ class Arg:
     name: str
     type: type
     description: str
+    expressions: list[str]
     required: bool
 
 
@@ -91,6 +93,7 @@ class Command:
     doc: str
     instance: Union[Controller, None]
     visible: bool
+    examples: list[str]
 
 
 class UI:
@@ -149,6 +152,7 @@ class UI:
             func=self.help,
             args=[],
             doc=str(self.help.__doc__).strip(),
+            examples=[],
             instance=None,
             visible=True,
         )
@@ -159,6 +163,7 @@ class UI:
             func=self.help,
             args=[],
             doc=str(self.help.__doc__).strip(),
+            examples=[],
             instance=None,
             visible=False,
         )
@@ -169,6 +174,7 @@ class UI:
             func=self.exit,
             args=[],
             doc=str(self.exit.__doc__).strip(),
+            examples=[],
             instance=None,
             visible=True,
         )
@@ -179,6 +185,7 @@ class UI:
             func=self.exit,
             args=[],
             doc=str(self.exit.__doc__).strip(),
+            examples=[],
             instance=None,
             visible=False,
         )
@@ -206,7 +213,10 @@ class UI:
             parameters = list(signature.parameters.values())[1:]
 
             args = []
-            for param in parameters:
+            for index, param in enumerate(parameters):
+                # enumerate so we can get a unique int for each int arg
+                # in the help text.
+                expressions = []
                 required = False
                 description = ""
                 if param.default == param.empty:
@@ -216,30 +226,65 @@ class UI:
                         # *args are optional, and any
                         # number of them may be provided.
                         description = f"[<{param.name}> ... ]"
+                        expressions.append("")
+                        expressions.append(f"{param.name}1")
+                        expressions.append(f"{param.name}1 {param.name}2")
 
                     elif param.kind == inspect.Parameter.VAR_KEYWORD:
                         # **kwargs are optional, but there's no way to know
                         # which kwargs are accepted. Just hint at the collective.
                         description = f"[{param.name}=<value>]"
+                        expressions.append("")
+                        expressions.append("undocumnted_arg1=some_value")
+                        expressions.append(
+                            "undocumented_arg1=some_value undocumented_arg2=some_value"
+                        )
 
-                    else:
-                        description = f"<{param.name}>"
-                        required = True
+                    elif t := type_hints.get(param.name, None):
+                        # If the argument is an enum, only provide the explicit values that
+                        # the enum can represent. Show these as (state1|state2|state3).
+                        if isinstance(t, EnumMeta):
+                            description = "(" + "|".join([e.value for e in t]) + ")"
+                            for e in t:
+                                expressions.append(e.value)
 
-                if t := type_hints.get(param.name, None):
-                    # If the argument is an enum, only provide the explicit values that
-                    # the enum can represent. Show these as (state1|state2|state3).
-                    if isinstance(t, EnumMeta):
-                        description = "(" + "|".join([e.value for e in t]) + ")"
+                        else:
+
+                            description = f"<{param.name}>"
+                            required = True
+                            t = type_hints.get(param.name, None)
+                            if t is Union[int, str]:
+                                expressions.append(f"{index}")
+                                # guh, business logic in the UI class :c
+                                expressions.append("all")
+                            elif t is int:
+                                # TODO: Make this say 3 if it's not the first int
+                                expressions.append(f"{index}")
+                            elif t is str:
+                                expressions.append("some_text")
+                            else:
+                                expressions.append(param.name)
 
                 args.append(
                     Arg(
                         name=param.name,
                         type=param.annotation,
                         description=description,
+                        expressions=expressions,
                         required=required,
                     )
                 )
+
+            examples = set()
+            expressions_lists = [arg.expressions for arg in args]
+            product_combinations = product(*expressions_lists)
+            for combination in product_combinations:
+                examples.add(f"{name} {' '.join(combination)}".strip())
+            examples = sorted(list(examples))
+
+            # Get rid of examples that are just the command name alone.
+            if len(examples) == 1 and examples[0] == name:
+                examples = []
 
             self.command[name] = Command(
                 name=name,
@@ -248,15 +293,17 @@ class UI:
                 doc=str(func.__doc__).strip(),
                 instance=self.controller,
                 visible=True,
+                examples=examples,
             )
 
     def help(self):
         """
-        Show this menu
+        Show this menu.
         """
         column_cmd = []
         column_arg = []
         column_doc = []
+        example_doc = []
 
         for name, command in sorted(self.command.items()):
             if not command.visible:
@@ -264,12 +311,15 @@ class UI:
             column_cmd.append(name)
             column_arg.append(" ".join([arg.description for arg in command.args]))
             column_doc.append(command.doc)
+            example_doc.append(command.examples)
 
         pad_cmd = max(len(cmd) for cmd in column_cmd) + 1
         pad_arg = max(len(arg) for arg in column_arg) + 1
 
         out = "\n"
-        for cmd, arg, doc in zip(column_cmd, column_arg, column_doc):
+        for cmd, arg, doc, examples in zip(
+            column_cmd, column_arg, column_doc, example_doc
+        ):
             line = f"{cmd}{' ' * (pad_cmd - len(cmd))}{arg}{' ' * (pad_arg - len(arg))}"
             # Treat linebreaks, tabs and multiple spaces as a single space.
             docstring = " ".join(doc.split())
@@ -282,6 +332,16 @@ class UI:
                 )
                 + "\n"
             )
+            for example in examples:
+                out += (
+                    textwrap.fill(
+                        f"- `{example}`",
+                        initial_indent=" " * (pad_cmd + pad_arg) + "    ",
+                        subsequent_indent=" " * (pad_cmd + pad_arg) + "    ",
+                        width=100,
+                    )
+                    + "\n"
+                )
         print(out)
         try:
             input("[Enter] ")
@@ -291,7 +351,7 @@ class UI:
 
     def exit(self):
         """
-        Quit
+        Quit.
         """
         sys.exit(0)
 

ammo/mod_controller.py
@@ -510,7 +510,7 @@ class ModController(Controller):
 
     def configure(self, index: int) -> None:
         """
-        Configure a fomod
+        Configure a fomod.
         """
         # This has to run a hard refresh for now, so warn if there are uncommitted changes
         if self.changes is True:
@@ -556,7 +556,7 @@ class ModController(Controller):
 
     def activate(self, component: ComponentEnum, index: Union[int, str]) -> None:
         """
-        Enabled components will be loaded by game
+        Enabled components will be loaded by game.
         """
         try:
             int(index)
@@ -586,7 +586,7 @@ class ModController(Controller):
 
     def deactivate(self, component: ComponentEnum, index: Union[int, str]) -> None:
         """
-        Disabled components will not be loaded by game
+        Disabled components will not be loaded by game.
         """
         try:
             int(index)
@@ -608,7 +608,7 @@ class ModController(Controller):
 
     def sort(self) -> None:
         """
-        Arrange plugins by mod order
+        Arrange plugins by mod order.
         """
         plugins = []
         for mod in self.mods[::-1]:
@@ -634,7 +634,7 @@ class ModController(Controller):
 
     def rename(self, component: RenameEnum, index: int, name: str) -> None:
         """
-        Names may contain alphanumerics and underscores
+        Names may contain alphanumerics and underscores.
         """
         if component not in list(RenameEnum):
             raise Warning(
@@ -718,7 +718,7 @@ class ModController(Controller):
 
     def delete(self, component: DeleteEnum, index: Union[int, str]) -> None:
         """
-        Removes specified file from the filesystem
+        Removes specified file from the filesystem.
         """
         if not isinstance(component, DeleteEnum):
             raise TypeError(
@@ -868,7 +868,7 @@ class ModController(Controller):
 
     def install(self, index: Union[int, str]) -> None:
         """
-        Extract and manage an archive from ~/Downloads
+        Extract and manage an archive from ~/Downloads.
         """
         if self.changes is True:
             raise Warning("You must `commit` changes before installing a mod.")
@@ -961,7 +961,7 @@ class ModController(Controller):
 
     def move(self, component: ComponentEnum, index: int, new_index: int) -> None:
         """
-        Larger numbers win file conflicts
+        Larger numbers win file conflicts.
         """
         if not isinstance(component, ComponentEnum):
             raise TypeError(
@@ -993,7 +993,7 @@ class ModController(Controller):
 
     def commit(self) -> None:
         """
-        Apply pending changes
+        Apply pending changes.
         """
         self._save_order()
         stage = self._stage()
@@ -1028,13 +1028,13 @@ class ModController(Controller):
 
     def refresh(self) -> None:
         """
-        Abandon pending changes
+        Abandon pending changes.
         """
         self.__init__(self.downloads_dir, self.game, *self.keywords)
 
     def collisions(self, index: int) -> None:
         """
-        Show file conflicts for a mod
+        Show file conflicts for a mod.
         """
         try:
             subject = self.mods[index]
@@ -1086,7 +1086,7 @@ class ModController(Controller):
 
     def find(self, *keyword: str) -> None:
         """
-        Show only components with any keyword
+        Show only components with any keyword. Execute without args to show all.
         """
         self.keywords = [*keyword]
 
@@ -1145,7 +1145,7 @@ class ModController(Controller):
 
     def tools(self) -> None:
         """
-        Manage tools
+        Manage tools.
         """
         if self.changes:
             raise Warning("You must commit changes before accessing tools")

ammo/tool_controller.py
@@ -145,7 +145,7 @@ class ToolController(Controller):
 
     def rename(self, component: ToolEnum, index: int, name: str) -> None:
         """
-        Names may contain alphanumerics or underscores
+        Names may contain alphanumerics or underscores.
         """
         if component not in list(ToolEnum):
             raise Warning(
@@ -208,7 +208,7 @@ class ToolController(Controller):
 
     def delete(self, component: ToolEnum, index: Union[int, str]) -> None:
         """
-        Removes specified file from the filesystem
+        Removes specified file from the filesystem.
         """
         if not isinstance(component, ToolEnum):
             raise TypeError(
@@ -270,7 +270,7 @@ class ToolController(Controller):
 
     def install(self, index: Union[int, str]) -> None:
         """
-        Extract and manage an archive from ~/Downloads
+        Extract and manage an archive from ~/Downloads.
         """
         try:
             int(index)
@@ -349,12 +349,12 @@ class ToolController(Controller):
 
     def mods(self) -> None:
         """
-        Return to the mod controller
+        Return to the mod controller.
         """
         self.do_exit = True
 
     def refresh(self) -> None:
         """
-        Scan for tools in the tool folder
+        Scan for tools in the tool folder.
         """
         self.__init__(self.downloads_dir, self.tools_dir)

commit 60a171094dda5283844ec6615b63d685d1b090da

Fix bugs from commit 90a53f1
Commit 90a53f1 caused arguments that were Enums to not be marked as
required. This broke the validation logic for number-of-args as well as
broke type-casting even when args were correct.

Fix both of these bugs by marking Enum args as required.

ammo/ui.py
@@ -240,30 +240,32 @@ class UI:
                             "undocumented_arg1=some_value undocumented_arg2=some_value"
                         )
 
-                    elif t := type_hints.get(param.name, None):
-                        # If the argument is an enum, only provide the explicit values that
-                        # the enum can represent. Show these as (state1|state2|state3).
-                        if isinstance(t, EnumMeta):
-                            description = "(" + "|".join([e.value for e in t]) + ")"
-                            for e in t:
-                                expressions.append(e.value)
-
+                    elif (t := type_hints.get(param.name, None)) and not isinstance(
+                        t, EnumMeta
+                    ):
+                        description = f"<{param.name}>"
+                        required = True
+                        t = type_hints.get(param.name, None)
+                        if t is Union[int, str]:
+                            expressions.append(f"{index}")
+                            # guh, business logic in the UI class :c
+                            expressions.append("all")
+                        elif t is int:
+                            # TODO: Make this say 3 if it's not the first int
+                            expressions.append(f"{index}")
+                        elif t is str:
+                            expressions.append("some_text")
                         else:
+                            expressions.append(param.name)
 
-                            description = f"<{param.name}>"
-                            required = True
-                            t = type_hints.get(param.name, None)
-                            if t is Union[int, str]:
-                                expressions.append(f"{index}")
-                                # guh, business logic in the UI class :c
-                                expressions.append("all")
-                            elif t is int:
-                                # TODO: Make this say 3 if it's not the first int
-                                expressions.append(f"{index}")
-                            elif t is str:
-                                expressions.append("some_text")
-                            else:
-                                expressions.append(param.name)
+                if t := type_hints.get(param.name, None):
+                    # If the argument is an enum, only provide the explicit values that
+                    # the enum can represent. Show these as (state1|state2|state3).
+                    required = True
+                    if isinstance(t, EnumMeta):
+                        description = "(" + "|".join([e.value for e in t]) + ")"
+                        for e in t:
+                            expressions.append(e.value)
 
                 args.append(
                     Arg(
@@ -424,6 +426,7 @@ class UI:
             # Validate that we received a sane number of arguments.
             num_required_args = len([arg for arg in command.args if arg.required])
             num_optional_args = len([arg for arg in command.args if not arg.required])
+
             if (
                 num_required_args > len(args)
                 or (num_required_args > len(args) and num_optional_args == 0)
@@ -454,7 +457,7 @@ class UI:
                         expected_arg = expected_args.pop(0)
 
             except (ValueError, KeyError) as e:
-                print(f"arg was unexpected type: {e}")
+                print(f"arg '{e}' was unexpected type: {type(e)}")
                 input("[Enter] ")
                 continue
 
@@ -474,3 +477,8 @@ class UI:
             except Warning as warning:
                 print(f"\n{warning}")
                 input("[Enter] ")
+
+            except Exception as e:
+                print(e)
+                print(f"{prepared_args=}")
+                raise