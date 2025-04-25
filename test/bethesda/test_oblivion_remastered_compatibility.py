babe03acc0fc5719c985e0b466be7e5b03775089
cyberrumor <cyberrumor@icloud.com>
2025-04-25 14:11:15 -0700

    Add directory structure of mod as a comment for oblivion tests

--- test/bethesda/test_oblivion_remastered_compatibility.py
+++ test/bethesda/test_oblivion_remastered_compatibility.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 from pathlib import Path
 
-import pytest
-
 from bethesda_common import (
     mod_extracts_files,
     mod_installs_files,
@@ -13,6 +11,15 @@ def test_extract_pak_root():
     """
     Test that installing a pak mod which contains all the directories
     up to and including ~mods is extracted without modification.
+    .
+    ├── pak_root.7z
+    │   └── OblivionRemastered
+    │       └── Content
+    │           └── Paks
+    │               └── ~mods
+    │                   ├── test.pak
+    │                   ├── test.ucas
+    │                   └── test.utoc
     """
     files = [
         Path("OblivionRemastered/Content/Paks/~mods/test.pak"),
@@ -27,6 +34,15 @@ def test_activate_pak_root():
     Test that activating a pak mod which contains all the directories
     up to and including ~mods causes files to be installed in the
     correct location under the game directory.
+    .
+    ├── pak_root.7z
+    │   └── OblivionRemastered
+    │       └── Content
+    │           └── Paks
+    │               └── ~mods
+    │                   ├── test.pak
+    │                   ├── test.ucas
+    │                   └── test.utoc
     """
     files = [
         Path("OblivionRemastered/Content/Paks/~mods/test.pak"),
