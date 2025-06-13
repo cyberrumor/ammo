978e8b8f75cb4b2d53f511bddfb8b1eae19c0fc5
cyberrumor <cyberrumor@icloud.com>
2025-06-13 15:40:51 -0700

    Add plural mod autocompletion tests for ModController
    Add autocompletion tests for ModController when there are multiple mods
    installed. Singular mods and absence of mods were already tested under
    commit 5e3c169.
    
    This doesn't add autocomplete tests for other controllers.
    
    After this patch, these completion scenarios have coverage for
    ModController:
    
    - Downloads are present
        - Mods are absent
        - A singular mod is present
        - Multiple mods are present

--- test/mod/test_autocomplete.py
+++ test/mod/test_autocomplete.py
@@ -8,6 +8,7 @@ from ammo.controller.mod import ModController
 from mod_common import (
     AmmoController,
     extract_mod,
+    install_mod,
 )
 
 NUM_DOWNLOADS = 28
@@ -151,9 +152,11 @@ class TestAutocompleteModSingular:
             # "activate " with mods instaled autocomplete "mod " since it's the only available
             # component type for the generic ModController (and it's going to have a valid target).
             ("", "activate ", ["mod "]),
+            ("", "activate mod ", ["0 "]),
             # "deactivate " with mods installed should autocomplete "mod " since it's the only available
             # component type for the generic ModController (and it's going to have a valid target).
             ("", "deactivate ", ["mod "]),
+            ("", "deactivate mod ", ["0 "]),
             # "delete " with mods installed should autocomplete "download " and "mod " since there's
             # more than one component type with valid targets.
             ("", "delete ", ["mod ", "download "]),
@@ -175,10 +178,12 @@ class TestAutocompleteModSingular:
             ("", "configure ", ["0 "]),
             # "move " with a mods installed should autocomplete the "mod " component.
             ("", "move ", ["mod "]),
+            ("", "move mod ", ["0 "]),
             # "rename " with mods installed should autocomplete valid component names.
             ("", "rename ", ["mod ", "download "]),
             # "rename m" with a mod installed should autocomplete the "mod " component.
             ("", "rename m", ["mod "]),
+            ("", "rename mod ", ["0 "]),
             # "rename d" should autocomplete the "download " component.
             ("", "rename d", ["download "]),
             # "rename download " should autocomplete download list but not "all"
@@ -195,6 +200,108 @@ class TestAutocompleteModSingular:
         ids=ids_hook,
     )
     def test_autocomplete_mods_present_singular(
+        self, text: str, buf: str, expected: list[str]
+    ):
+        with patch("readline.get_line_buffer", return_value=buf):
+            results = []
+            state = 0
+            while (result := self.complete(text, state)) is not None:
+                results.append(result)
+                state += 1
+
+            assert results == expected
+
+
+class TestAutocompleteModPlural:
+    @pytest.fixture(scope="class", autouse=True)
+    def setup_controller(self, request):
+        with AmmoController() as controller:
+            request.cls.controller = controller
+            with patch.object(
+                ModController, "has_extra_folder", return_value=True
+            ) as _mock_has_extra_folder:
+                install_mod(controller, "mock_conflict_1")
+                install_mod(controller, "mock_conflict_2")
+                install_mod(controller, "normal_mod")
+
+            def complete(self, text: str, state: int) -> Union[str, None]:
+                try:
+                    return request.cls.controller.autocomplete(text, state)
+                except Exception:
+                    #  Any exception raised during the evaluation of the expression is caught,
+                    # silenced and None is returned.
+                    # https://docs.python.org/3/library/rlcompleter.html#rlcompleter.Completer
+                    return None
+
+            request.cls.complete = complete
+
+            yield
+
+    @pytest.mark.parametrize(
+        "text, buf, expected",
+        [
+            # Note that completing the first command names is handled by readline magic,
+            # so the autocomplete function in the controllers don't have to do anything
+            # special for them. It also means we can't test that behavior here since our
+            # autocompleters only do stuff after the command name has been passed in.
+            # Install any specific download via number or "all"
+            (
+                "",
+                "install ",
+                [*(str(i) + " " for i in range(0, NUM_DOWNLOADS + 1)), "all "],
+            ),
+            # "activate " with mods instaled autocomplete "mod " since it's the only available
+            # component type for the generic ModController (and it's going to have a valid target).
+            ("", "activate ", ["mod "]),
+            ("", "activate mod ", ["0 ", "1 ", "2 ", "all "]),
+            # "deactivate " with mods installed should autocomplete "mod " since it's the only available
+            # component type for the generic ModController (and it's going to have a valid target).
+            ("", "deactivate ", ["mod "]),
+            ("", "deactivate mod ", ["0 ", "1 ", "2 ", "all "]),
+            # "delete " with mods installed should autocomplete "download " and "mod " since there's
+            # more than one component type with valid targets.
+            ("", "delete ", ["mod ", "download "]),
+            # "delete m" with mods installed should autocomplete the "mod " component name.
+            ("", "delete m", ["mod "]),
+            # "delete mod " with multiple mods installed should autocomplete all available indices and "all ".
+            ("", "delete mod ", ["0 ", "1 ", "2 ", "all "]),
+            # "delete download " should autocomplete download indices and "all ".
+            (
+                "",
+                "delete download ",
+                [*(str(i) + " " for i in range(0, NUM_DOWNLOADS + 1)), "all "],
+            ),
+            # "collisions " with mods that have conflicts should autocomplete only indices with collisions.
+            # E.g., don't complete the index for "normal_mod" here. Just collision_1 and collision_2.
+            ("", "collisions ", ["0 ", "1 "]),
+            # "configure " with mods installed should only autocomplete the index if the mod is a fomod.
+            # In this scenario, there are no fomods.
+            ("", "configure ", []),
+            # "move " with mods installed should autocomplete the "mod " component.
+            ("", "move ", ["mod "]),
+            ("", "move mod ", ["0 ", "1 ", "2 "]),
+            ("", "move mod 1 ", ["0 ", "1 ", "2 "]),
+            # "rename " with mods installed should autocomplete valid component names.
+            ("", "rename ", ["mod ", "download "]),
+            # "rename m" with mods installed should autocomplete the "mod " component.
+            ("", "rename m", ["mod "]),
+            ("", "rename mod ", ["0 ", "1 ", "2 "]),
+            # "rename d" should autocomplete the "download " component.
+            ("", "rename d", ["download "]),
+            # "rename download " should autocomplete download list but not "all"
+            (
+                "",
+                "rename download ",
+                [str(i) + " " for i in range(0, NUM_DOWNLOADS + 1)],
+            ),
+            # "delete p" should NOT autocomplete "plugin " since it's a bogus component type for this controller.
+            # For some reason this wants to autocomplete ["mod ", "download "] even though the actual observed
+            # and desired behavior is empty list here. Weird.
+            # ("", "delete p", []),
+        ],
+        ids=ids_hook,
+    )
+    def test_autocomplete_mods_present_plural(
         self, text: str, buf: str, expected: list[str]
     ):
         with patch("readline.get_line_buffer", return_value=buf):
